f4e30b1f3774af538cdc4631c9641ad2dfa823ef
Andrew Boryk <andrewcboryk@gmail.com>
2015-01-26 20:24:07 -0500

    Initial Commit

---
+++ justgo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  justgo
+//
+//  Created by Andrew Boryk on 1/26/15.
+//  Copyright (c) 2015 Andrew Boryk. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ justgo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  justgo
+//
+//  Created by Andrew Boryk on 1/26/15.
+//  Copyright (c) 2015 Andrew Boryk. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
